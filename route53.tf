770a46ef3eb649f580a888f9522933b22f7958c0
Terraform <noreply@codingones.com>
2023-04-21 22:56:41 +0200

    feat: adding terraform aws service file

--- route53.tf
+++ route53.tf
@@ -1,11 +1,18 @@
+locals {
+  dkim_tokens = {
+    token1 = aws_sesv2_email_identity.email_identity.dkim_signing_attributes[0].tokens[0]
+    token2 = aws_sesv2_email_identity.email_identity.dkim_signing_attributes[0].tokens[1]
+    token3 = aws_sesv2_email_identity.email_identity.dkim_signing_attributes[0].tokens[2]
+  }
+}
+
+
 resource "aws_route53_record" "ses_cnames" {
-  for_each = tolist(aws_sesv2_email_identity.email_identity.dkim_signing_attributes[0].tokens)
+  for_each = local.dkim_tokens
 
   zone_id = var.hosting_zone_id
   name    = "${each.value}._domainkey.${var.domain_name}"
   type    = "CNAME"
   ttl     = "300"
   records = ["${each.value}.dkim.amazonses.com"]
-
-  depends_on = [aws_sesv2_email_identity.email_identity]
 }
