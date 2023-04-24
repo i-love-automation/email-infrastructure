4d40d70add1f6954ecfaa70d7dde17fbba232376
Terraform <noreply@codingones.com>
2023-04-24 12:20:48 +0200

    feat: adding terraform aws service file

--- ses.tf
+++ ses.tf
@@ -33,3 +33,19 @@ resource "aws_sesv2_configuration_set" "ses_configuration" {
 
   tags = local.tags
 }
+
+
+resource "aws_ses_receipt_rule_set" "rule_set" {
+  rule_set_name = "email_forwarding_rule_set"
+}
+
+resource "aws_ses_receipt_rule" "email_forwarding" {
+  name          = "email_forwarding"
+  rule_set_name = aws_ses_receipt_rule_set.rule_set.rule_set_name
+  recipients    = ["*@${var.domain_name}"]
+
+  lambda_action {
+    position     = 1
+    function_arn = aws_lambda_function.email_forwarding.arn
+  }
+}
