31e6da4cdf2c78e175fb9d2bd3c45a734246702b
Romain Cambonie <romain.cambonie@gmail.com>
2023-04-25 13:19:30 +0200

    Update execute.sh

--- lambda/execute.sh
+++ lambda/execute.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Change to the desired directory where package.json is located
-pwd
+#pwd
 
-ls -la
+ls -la /usr/local/bin/
 
 # Run npm install
-npm install
+/usr/local/bin/npm install
